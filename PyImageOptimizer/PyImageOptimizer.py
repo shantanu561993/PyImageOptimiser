2efbe4f6a8a4c33d538f76a3863cb1030e53abee
Shantanu Khandelwal <shantanu561993@gmail.com>
2015-07-14 23:41:38 +0530

    Added ThreadPool Implementation , added optimize method

--- PyImageOptimizer/PyImageOptimizer.py
+++ PyImageOptimizer/PyImageOptimizer.py
@@ -58,5 +58,14 @@ def optimize_png(file_path,backup=False):
 					 optimize_png(os.path.abspath(os.path.join(root,file)),backup)
 
 
-	
+
+def optimize(file_path,backup=False):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        for root,dirs,files in os.walk(file_path):
+            for file in files:
+                if file.endswith(".jpg"):
+                    executor.submit(optimize_jpg,file_path,backup)
+                elif file.endswith(".png"):
+                    executor.submit(optimize_png,file_path,backup)
+                                
 																																																																																																																																																																																																																																			
--- PyImageOptimizer/__init__.py
+++ PyImageOptimizer/__init__.py
@@ -4,4 +4,4 @@ __author__ = 'Shantanu Khandelwal'
 __email__ = 'shantanu561993@gmail.com'
 __version__ = '1.0'
 
-from .PyImageOptimizer import optimise_jpg
+from .PyImageOptimizer import optimize_jpg,optimize_png,optimize
