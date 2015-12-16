70fd55d1a83b02990fc8e34913682dc7fc804b6e
Shantanu Khandelwal <shantanu561993@gmail.com>
2015-12-16 17:10:45 +0530

    fixed some indentation problem .. Duh !

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 setup(
   name = 'PyImageOptimizer',
   packages = ['PyImageOptimizer'], # this must be the same as the name above
-  version = '1.0',
+  version = '1.1',
   description = 'An Image Optimiser  ',
   author = 'Shantanu Khandelwal',
   author_email = 'shantanu561993@gmail.com',
   url = 'https://github.com/shantanu561993/PyImageOptimiser', # use the URL to the github repo
-  download_url = 'https://github.com/shantanu561993/PyImageOptimiser/tarball/1.0',
+  download_url = 'https://github.com/shantanu561993/PyImageOptimiser/tarball/1.1',
   keywords = ['Image', 'optimize', 'compress','image','jpeg','png','optimise','crush'], # arbitrary keywords
 )
